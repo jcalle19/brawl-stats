a3668cb28986f570daf93fa0f3b5eb10134906d4
jcalle19 <jcalle19@asu.edu>
2026-05-12 23:16:01 -0700

    Added polling function to util_function.js
    Added function that periodically sends api request to brawlAPI to grab current battlelog. The function then grabs the currently untracked games in order to send to the database.

--- express-server/socket_handler.js
+++ express-server/socket_handler.js
@@ -1,7 +1,8 @@
 import { util } from './util_functions.js';
 
 export const socket_functions = (io) => {
-
+    util.poll_player_data();
+    
     io.on('connection', (socket) => {
         const temp_cards = [];
 
--- express-server/util_functions.js
+++ express-server/util_functions.js
@@ -12,6 +12,8 @@ const dbClient = createClient(
   process.env.SUPABASE_SECRET_KEY
 );
 
+let recentTime = '20260511T172413.000Z';
+let i = 0;
 const safe = (handler) => {
     return async (...args) => {
         try {
@@ -20,6 +22,13 @@ const safe = (handler) => {
             console.error('Socket handler error:', err);
         }
     };
+}
+
+const poll_player_data = async () => {
+    console.log('polling', i); i++;
+    let battle_log = await get_brawl_data();
+    console.log(trim_games(parse_battle_time(recentTime), battle_log.items).length); //replace with db insertion
+    setTimeout(poll_player_data, 100000/*2700000*/);
 }
 
 const connection = async (io, socket) => {
@@ -51,9 +60,44 @@ const db_insertion = async (id, newInfo, temp_cards) => {
     temp_cards.length = 0;
 }
 
+const get_brawl_data = async () => {
+    const id = process.env.TEST_ID; //params;
+    const result = await fetch(`https://bsproxy.royaleapi.dev/v1/players/%23${id}/battlelog`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${process.env.BRAWL_API_KEY}`,
+        },
+    });
+    const data = await result.json();
+    return data;
+}
+
+//grab only untracked games from battlelog
+const trim_games = (mostRecentTime, games) => {
+    const untracked = [];
+    let currTime;
+    for(let i = 0; i < games.length; i++) {
+        currTime = parse_battle_time(games[i].battleTime);
+        if (currTime > mostRecentTime) untracked.push(games[i]);
+        else break;
+    }
+    if (untracked[0]) recentTime = untracked[0].battleTime; //replace with db query to get players.id.mostrecent
+    return untracked;
+}
+
+const parse_battle_time = (battleTime) => {
+    //let battleTime = '20260510T120000.000Z';
+    const formatted = battleTime.replace(/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})/, '$1-$2-$3T$4:$5:$6');
+    return new Date(formatted);
+}
+
 export const util = {
     dbClient,
     authClient,
     safe,
     connection,
+    get_brawl_data,
+    parse_battle_time,
+    poll_player_data,
 }
