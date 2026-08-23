2961238d8b0b281a82ad4b1d26c428de348d7f93
jcalle19 <jcalle19@asu.edu>
2026-08-22 21:59:55 -0700

    Updated mobile view
    Added separate portion for mobile view

--- stats-frontend/components/placeholder-components/defaultInfoPanel.jsx
+++ stats-frontend/components/placeholder-components/defaultInfoPanel.jsx
@@ -14,8 +14,8 @@ import { portraitURLs } from '@/public/portaitURLMap.js'
 
 const DefaultInfoPanel = () => {
     const {selectedMatch, focusedStats} = useMatchContext();
-    return (
-        <div className='relative w-full h-[150%] md:h-full select-none grid grid-rows-[1fr_7fr] p-5 pt-15'>
+    /* 
+    <div className='relative w-full h-[150%] md:h-full select-none grid grid-rows-[1fr_7fr] p-5 pt-15'>
             <GridSection classes={'grid grid-cols-2 md:grid-cols-[3fr_1fr] min-h-[3rem] gap-8'}>
                 <GridSection classes={''}>
                     <div className='relative-center huge-text'>Match Overview ID:{selectedMatch?.player_id}</div>
@@ -26,7 +26,7 @@ const DefaultInfoPanel = () => {
             </GridSection>
             <GridSection classes={'grid grid-rows-[fit_500px] md:grid-cols-[3fr_1fr] min-h-[25rem] gap-8'}>
                 <GridSection classes={'md:row-span-2 grid grid-rows-[1fr_2fr]'}>
-                        <GridSection classes={'grid grid-rows-2 md:grid-rows-[3fr_105fr]'}>
+                        <GridSection classes={'grid grid-rows-2 md:grid-rows-[3fr_5fr]'}>
                             <GridSection><MatchInfoRow selected={selectedMatch}/></GridSection>
                             <GridSection><PlayerRow selected={selectedMatch}/></GridSection>
                         </GridSection>
@@ -50,6 +50,65 @@ const DefaultInfoPanel = () => {
                 </GridSection>
             </GridSection>
         </div>
+    */
+    return (
+        <>
+            <div className='hidden relative w-full h-[150%] md:h-full select-none md:grid grid-rows-[1fr_7fr] p-5 pt-15'>
+                <GridSection classes={'grid grid-cols-2 md:grid-cols-[3fr_1fr] min-h-[3rem] gap-8'}>
+                    <GridSection classes={''}>
+                        <div className='relative-center huge-text'>Match Overview ID:{selectedMatch?.player_id}</div>
+                    </GridSection>
+                    <GridSection classes={'striped-bg rounded'}>
+                        <div className='relative-center w-[75%] h-[25px]'><SearchBar/></div>
+                    </GridSection>
+                </GridSection>
+                <GridSection classes={'grid grid-rows-[fit_500px] md:grid-cols-[3fr_1fr] min-h-[25rem] gap-8'}>
+                    <GridSection classes={'md:row-span-2 grid grid-rows-[1fr_2fr]'}>
+                            <GridSection classes={'grid grid-rows-2 md:grid-rows-[3fr_5fr]'}>
+                                <GridSection><MatchInfoRow selected={selectedMatch}/></GridSection>
+                                <GridSection><PlayerRow selected={selectedMatch}/></GridSection>
+                            </GridSection>
+                        <GridSection classes={'flex'}>
+                            <div className='relative grid grid-rows-[1fr_12fr]'>
+                                <GridSection classes={'purple'}>
+                                    <div className='relative-center text-fit-sm font-bold italic'>
+                                        {selectedMatch?.map}
+                                    </div>
+                                </GridSection>
+                                <MapBase selectedMap={selectedMatch?.map}/>
+                            </div>
+                            <GridSection classes={'grid grid-rows-2 test-border'}>
+                                <GridSection classes={'striped-bg'}><MatchEloInfo selected={selectedMatch}/></GridSection>
+                                <GridSection classes={''}><DonutRow focused={focusedStats}/></GridSection>
+                            </GridSection>
+                        </GridSection>
+                    </GridSection>
+                    <GridSection classes={'md:row-span-2'}>
+                        <TopPlayerStats username={'Kriby'} focusedStats={focusedStats} selectedMatch={selectedMatch}/>
+                    </GridSection>
+                </GridSection>
+            </div>
+            <div className='md:hidden relative w-full h-fit md:h-full select-none grid md:grid-rows-[1fr_7fr] md:grid-cols-[3fr_1fr] p-5 pt-15 test-border'>
+                <GridSection id='mobile-view' classes={'block md:hidden grid grid-rows-[75px_75px_auto_auto_auto_auto_500px] row-span-2'}>
+                    <GridSection classes={'grid grid-cols-2 md:grid-cols-[3fr_1fr] min-h-[3rem] max-h-[75px] gap-8'}>
+                        <GridSection classes={''}>
+                            <div className='relative-center text-fit'>Match Overview ID:{selectedMatch?.player_id}</div>
+                        </GridSection>
+                        <GridSection classes={'striped-bg rounded'}>
+                            <div className='relative-center w-[75%] h-[25px]'><SearchBar/></div>
+                        </GridSection>
+                    </GridSection>
+                    <GridSection classes={'min-h-[3rem] max-h-[75px]'}><MatchInfoRow selected={selectedMatch}/></GridSection>
+                    <GridSection classes={''}><PlayerRow selected={selectedMatch}/></GridSection>
+                    <GridSection classes={'max-h-[500px] test-border'}></GridSection>
+                    <GridSection classes={'max-h-[500px] test-border'}></GridSection>
+                    <GridSection classes={'max-h-[500px]'}>
+                        <TopPlayerStats username={'Kriby'} focusedStats={focusedStats} selectedMatch={selectedMatch}/>
+                    </GridSection>
+                </GridSection>
+            </div>
+        </>
+        
     )
 }
 
--- stats-frontend/components/placeholder-components/playerRow.jsx
+++ stats-frontend/components/placeholder-components/playerRow.jsx
@@ -11,7 +11,7 @@ let boxToggles = [false,false,false,false,false];
 
 const PlayerRow = ({selected}) => {
     const [selectedIndex, setSelectedIndex] = useState(-1);
-    const commonTraits = 'h-full min-h-[100px] shrink-0 aspect-square rounded-[5px] overflow-hidden test-border'
+    const commonTraits = 'min-h-[100px] shrink-0 aspect-square rounded-[5px] overflow-hidden test-border'
 
     const handleClick = (index) => {
         console.log(selectedIndex, index);
@@ -19,7 +19,7 @@ const PlayerRow = ({selected}) => {
     }
 
   return (
-    <div className='stats-bg w-full md:flex md:justify-center md:overflow-x-auto grid grid-cols-3 grid-rows-3 gap-2 p-4 md:p-2 test-border'>
+    <div className='stats-bg h-full w-full md:min-w-fit md:flex md:justify-center md:overflow-x-auto grid grid-cols-3 grid-rows-2 md:grid-rows-1 md:grid-cols-6 gap-8 p-4 md:p-2 test-border'>
         <div className={`${commonTraits}`}>
             <BrawlIcons src={portraitURLs[selected?.brawler]} width={'100%'} size={portraitSize}/>
         </div>
@@ -33,9 +33,6 @@ const PlayerRow = ({selected}) => {
                 selectedId={selectedIndex} boxId={1}/>
             <BrawlIcons src={portraitURLs[selected?.team2?.brawler.name]} 
                 width={'100%'} size={portraitSize} onClick={()=>{boxToggles[1] = !boxToggles[1]}}/>
-        </div>
-        <div className={commonTraits}>
-            <div className='relative-center text-fit'>vs</div>
         </div>
         <div className={`${commonTraits} enemy-box`} onClick={()=>handleClick(2)}>
             <ToggleBox colorClass={'red'} content={[selected?.enemy1?.tag, selected?.enemy1?.name]} 
